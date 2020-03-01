61c8c73d5d6c3e536c4c6b5f0252c24beea7db16
hamzah <haldolaay@gmail.com>
2020-03-01 04:55:22 -0800

    init

---
+++ app.js
@@ -0,0 +1,64 @@
+//jshint esversion:6
+const express = require('express'); // server
+const bodyParser = require('body-parser'); // to handle requests 
+// const cookieParser = require('cookie-parser') //
+const ejs = require('ejs'); // to render html from inside javascript
+const app = express(); // starting the server
+const mysql = require('mysql') // database driver
+const session = require('express-session')// used for cookie handeling
+require('dotenv').config() // using environment variables
+// setting up express 
+app.use(express.static('/public')); // public path
+app.use(bodyParser.urlencoded({extended: true})); // parsing the request
+app.use(session({secret: "thisIsTopSecret"}))
+// app.use(cookieParser);
+app.set('view engine','ejs'); // using ejs
+
+
+
+
+
+// connection authintication
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: process.env.USER_NAME,
+    password: process.env.PASSWORD,
+    database: 'users',
+    insecureAuth : true
+  });
+  // connecting to mysql
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log(" mysql Connected!");
+
+  });
+app.get('/hello',(req,res)=>{
+  let sql = 'SELECT * FROM seasion';
+
+  connection.query(sql,(err,result)=>{console.log(result)})
+  let sees = req.session.page_views;
+  res.send(sees);
+})
+
+// creating the paths
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+//login path
+app.get('/login',(req,res)=>{
+    res.render('login')
+});
+app.post('/login',(req,res)=>{
+ console.log(req.body);
+ res.render('submit')
+})
+// regester path
+app.get('/register',(req,res)=>{
+    res.render('register')
+});
+
+// app listening to port 
+app.listen(3000,()=>{console.log('working!')})
+
+
